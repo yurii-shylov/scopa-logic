d5342538044f55aeb2954b3a12abdbccee314c02
yshylov <shilov.yuri@gmail.com>
2013-11-05 10:00:09 +0200

    Init project

---
+++ logic/src/main/java/com/deknaua/scopa/logic/Logic.java
@@ -0,0 +1,13 @@
+package com.deknaua.scopa.logic;
+
+import com.deknaua.scopa.state.State;
+
+public class Logic {
+  private State state;
+  
+  public Logic(State state) {
+    this.state = state;
+    
+    
+  }
+}
